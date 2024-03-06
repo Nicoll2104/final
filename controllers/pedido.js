1864435ee5aca25a49c19e6bb92b0d35e4aacff2
Nicoll2104 <soda210461@gmail.com>
2024-03-06 17:51:04 -0500

    first commit

---
+++ controllers/pedido.js
@@ -0,0 +1,26 @@
+import pedido from "../models/pedido.js";
+import { generarJWT } from "../middlewares/validar.js";
+import { json } from "body-parser";
+
+const httpPedido ={
+    getPedido: async (req,res)=>{
+        const pedidos = await pedido.find()
+        res.json(pedidos)
+    },
+
+    getPedidoid: async (req,res)=>{
+        const {id}=req.params
+        try{
+            const pedidos = await pedido.findById(id)
+            res.json({pedidos})
+        }catch(error){
+            res.status(400),json({error:'no encontramos el id'})
+        }
+    },
+
+    postPedido: async (req,res)=>{
+        try{
+            const {}
+        }
+    }
+}
---
+++ models/pedido.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const pedido = new mongoose.Schema(
+    {
+        fecha_creacion:{type:Date, required: true,},
+        fecha_entrga:{type:Date, required: true,},
+        subtotal:{type:String, required: true,},
+        impuestos:{type:String, required: true,},
+        total:{type:String, required: true,},
+        createdAt: { type: Date, default: Date.now },
+        status:{type:String,default:1}
+    })
+
+export default mongoose.model("Pedido", pedido)
