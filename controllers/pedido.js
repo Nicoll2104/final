3e52f2a37580aaa9544ae5b7ffbfa5b3071aa7f5
Nicoll2104 <you@example.com>
2024-03-07 11:14:34 -0500

    actual

--- controllers/pedido.js
+++ controllers/pedido.js
@@ -20,8 +20,65 @@ const httpPedido ={
 
     postPedido: async (req,res)=>{
         try{
+            const {fecha_creacion, fecha_entrga, subtotal, impuestos, total}=req.body
+            const pedidos = new pedido({fecha_creacion, fecha_entrga, subtotal, impuestos, total})
+        
+            await pedido.save()
+            res.json({mensaje: 'Pedido agregado con éxito'})
+        }catch(error){
+            res.status(400).json({error:'no encontramos el id'})
         }
-    }catch(error){
-        res.status(400).json({error:'no encontramos el id'})
-    }
+    },
+
+    putPedido: async (req,res)=>{
+        const {id}=req.params;
+        const {fecha_creacion, fecha_entrga, subtotal, impuestos, total}= req.body
+
+        try{
+            const pedidos = await pedido.findByIdAndUpdate(id,{fecha_creacion, fecha_entrga, subtotal, impuestos, total}, {new: true});
+
+            if(!pedidos){
+                return res.status(404).json({mensaje: 'El cliente no existe' })
+            }
+            res.json({ mensaje: 'Pedido actualizado con éxito', pedidos });
+        }catch(error){
+            res.status(500).json({ error: 'Error interno del servidor' });
+        }
+    },
+
+    deletePedido: async (req,res)=>{
+        try{
+            const {id} = req.params;
+            const pedidos = await pedido.findByIdAndDelete(id);
+
+            if(!pedidos){
+                return res.status(404).json({ mensaje: 'El pedido no existe' });
+            }
+            res.json({ mensaje: 'El pedido ha sido eliminado' });
+        }catch(error){
+            res.status(500).json({ error: 'Ocurrió un error al intentar eliminar el usuario' });
+        }
+    },
+
+    putInactivar: async (req,res)=>{
+        try{
+            const {id}=req.params
+            const pedidos = await pedido.findByIdAndUpdate(id,{status:0},{new:true})
+            res.json({pedidos})
+        }catch(error){
+            res.status(400).json({error: 'Se produjo un error'})
+        }
+    },
+
+    putActivar: async (req,res)=>{
+        try{
+            const {id}=req.params
+            const pedidos = await pedido.findByIdAndUpdate(id,{status:1},{new:true})
+            res.json({pedidos})
+        }catch(error){
+            res.status(400).json({error: 'Se produjo un error'})
+        }
+    },
 }
+
+export default httpPedido;
--- models/pedido.js
+++ models/pedido.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const pedido = new mongoose.Schema(
     {
         fecha_creacion:{type:Date, required: true,},
-        fecha_entrga:{type:Date, required: true,},
+        fecha_entrega:{type:Date, required: true,},
         subtotal:{type:String, required: true,},
         impuestos:{type:String, required: true,},
         total:{type:String, required: true,},
